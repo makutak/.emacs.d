26046fbb591f26bea961df8e2347585047bb9c89
makutak <kounotakuma@realglobe.jp>
2017-08-15 16:56:05 +0900

    修正

--- inits/20-web-mode.el
+++ inits/20-web-mode.el
@@ -1,6 +1,11 @@
 (require-or-install 'web-mode)
 
+(setq web-mode-auto-close-style 1)
+(setq web-mode-tag-auto-close-style t)
+
 (appendf auto-mode-alist
          '(("\\.\\(html?\\|emb\\|tmpl\\|tt\\)$" . web-mode)
+           ("\\.html$" . web-mode)
            ("\\.jsx$" . web-mode)
            ("\\.erb$" . web-mode)))
+
