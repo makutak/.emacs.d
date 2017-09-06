9f332ddbc816cd4d7e0016abaae7d6111f6c799b
makutak <kounotakuma@realglobe.jp>
2017-09-06 19:32:22 +0900

    jsでもreplを使いたい

--- inits/20-javascript.el
+++ inits/20-javascript.el
@@ -13,3 +13,12 @@
 (unless (package-installed-p 'coffee-mode)
   (package-install 'coffee-mode))
 (autoload 'coffee-mode "coffee-mode" nil t)
+
+(require-or-install 'nodejs-repl)
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-expression)
+            (define-key js-mode-map (kbd "C-c C-j") 'nodejs-repl-send-line)
+            (define-key js-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
+            (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
+            (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
