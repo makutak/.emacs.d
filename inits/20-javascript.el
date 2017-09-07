fd69d0a8aa6851fd147352785d18d3929eb898c9
makutak <kounotakuma@realglobe.jp>
2017-09-07 20:09:33 +0900

    indiumの設定を追加

--- inits/20-javascript.el
+++ inits/20-javascript.el
@@ -14,11 +14,11 @@
   (package-install 'coffee-mode))
 (autoload 'coffee-mode "coffee-mode" nil t)
 
-(require-or-install 'nodejs-repl)
+(require-or-install 'indium)
+(unless (package-installed-p 'indium)
+  (package-install 'indium))
+(autoload 'indium "indium" nil t)
+
 (add-hook 'js2-mode-hook
           (lambda ()
-            (define-key js-mode-map (kbd "C-x C-e") 'nodejs-repl-send-last-expression)
-            (define-key js-mode-map (kbd "C-c C-j") 'nodejs-repl-send-line)
-            (define-key js-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
-            (define-key js-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
-            (define-key js-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
+            (indium-interaction-mode)))
