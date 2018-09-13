f27c5b68a6d435d9af1cb96c9c9e577e58120699
makutak <t.kono@connectom.co.jp>
2018-09-13 13:17:19 +0900

    削除

--- init.el
+++ init.el
@@ -197,18 +197,3 @@
 (load "setup-css.el")
 (load "setup-haml.el")
 (load "setup-org-mode.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2)
- '(package-selected-packages
-   (quote
- '(ruby-insert-encoding-magic-comment nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
