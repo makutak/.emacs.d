ff9a74e1384f48f5bc5f278c27893c4a6c945b28
makutak <takkouno@gmail.com>
2019-05-20 21:29:54 +0900

    add js/ts config

--- inits/web.el
+++ inits/web.el
@@ -2,6 +2,7 @@
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-cssindent-offset 2)
   (setq web-mode-scssindent-offset 2)
+  (setq web-mode-code-indent-offset 2)
   (setq web-mode-script-padding 0)
   (setq web-mode-style-padding 0))
 
@@ -19,6 +20,7 @@
 
 (use-package web-mode
   :straight t
+  :mode ("\\.vue")
   :hook
   (web-mode . web-mode-hook)
   :config
@@ -45,3 +47,59 @@
 
 (use-package vue-mode
   :straight t)
+
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+(use-package typescript-mode
+  :config
+  (setq typescript-indent-level 2))
+
+
+(use-package tide
+  :straight t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save))
+  :config
+  (setq typescript-indent-level 2)
+  (setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions
+                              t
+                              :placeOpenBraceOnNewLineForFunctions
+                              nil))
+  )
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+
+
+(use-package js2-mode
+  :straight t
+  :disabled t
+  :mode ("\\.js")
+  :config
+  (add-hook 'js2-mode-hook #'setup-tide-mode))
+
+(use-package rjsx-mode
+  :straight t
+  :mode ("\\.js")
+  :config
+  (add-hook 'rjsx-mode-hook #'setup-tide-mode)
+  (flycheck-add-mode 'javascript-eslint 'rjsx-mode)
+  (setq indent-tabs-mode nil)
+  (setq js-indent-level 2))
