2749879232f89d11093aec9b49b5912e77ba3e15
makutak <takkouno@gmail.com>
2019-08-01 02:40:00 +0900

    configured rust mode

--- inits/rust.el
+++ inits/rust.el
@@ -1,1 +1,15 @@
-(use-package rust-mode)
+(use-package rust-mode
+  :config
+  (setq rust-format-on-save t))
+
+(use-package racer
+  :init
+  (add-hook 'rust-mode #'racer-mode)
+  (add-hook 'racer-mode #'eldoc-mode))
+
+(use-package flycheck-rust
+  :init
+  (add-hook 'rust-mode-hook
+            '(lambda ()
+               (flycheck-mode)
+               (flycheck-rust-setup))))
