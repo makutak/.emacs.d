44f9f427d7a2e656edef4bc9953b86c9c1307bc2
makutak <kouno@catallaxy.me>
2019-09-07 01:59:37 +0900

    fixed python config

--- inits/python.el
+++ inits/python.el
@@ -1,9 +1,17 @@
-(package-initialize)
-
 (use-package python-mode
-  :straight t)
+  :straight t
+  :config
+  (eldoc-mode t))
 
 (use-package elpy
   :straight t
   :init
-  (elpy-enable))
+  (elpy-enable)
+  :config
+  (setq python-shell-interpreter "ipython"
+        python-shell-interpreter-args "--simple-prompt -i"))
+
+(use-package py-yapf
+  :straight t
+  :hook
+  (python-mode . py-yapf-enable-on-save))
