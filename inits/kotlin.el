6a850c2a9674536786fa53133990461d9a1a851c
makutak <takkouno@gmail.com>
2020-01-24 01:59:41 +0900

    lsp使う

--- inits/kotlin.el
+++ inits/kotlin.el
@@ -1,8 +1,19 @@
-(use-package kotlin-mode)
+(add-to-list 'exec-path "~/work/kotlin-language-server/server/build/install/server/bin")
 
+(use-package kotlin-mode)
 (use-package flycheck-kotlin
 	:after kotlin-mode
 	:init
 	(flycheck-kotlin-setup))
 
-(use-package eglot)
+(use-package lsp-mode
+  :hook (kotlin-mode . lsp)
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package company-lsp :commands company-lsp)
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+;; optionally if you want to use debugger
+(use-package dap-mode)
