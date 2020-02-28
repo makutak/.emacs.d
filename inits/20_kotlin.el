a77fb8fde3f00d9ddc3b15acb2d3c7b5ebbff913
makutak <kouno@catallaxy.me>
2020-02-28 15:04:54 +0900

    lspまとめる

--- inits/20_kotlin.el
+++ inits/20_kotlin.el
@@ -5,15 +5,3 @@
 	:after kotlin-mode
 	:init
 	(flycheck-kotlin-setup))
-
-(use-package lsp-mode
-  :hook (kotlin-mode . lsp)
-  :commands lsp)
-
-;; optionally
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package company-lsp :commands company-lsp)
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-;; optionally if you want to use debugger
-(use-package dap-mode)
--- inits/20_python.el
+++ inits/20_python.el
@@ -1,20 +1,22 @@
 (use-package python-mode
   :straight t
+  :hook
+  (python-mode . lsp)
   :config
   (eldoc-mode t)
   (setq python-indent-offset 4))
 
-(use-package elpy
-  :straight t
-  ;;:ensure t
-  :defer t
-  :init
-  (advice-add 'python-mode :before 'elpy-enable)
-  :config
-  (setq elpy-rpc-virtualenv-path 'current)
-  (when (load "flycheck" t t)
-    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-    (add-hook 'elpy-mode-hook 'flycheck-mode)))
+;; (use-package elpy
+;;   :straight t
+;;   ;;:ensure t
+;;   :defer t
+;;   :init
+;;   (advice-add 'python-mode :before 'elpy-enable)
+;;   :config
+;;   (setq elpy-rpc-virtualenv-path 'current)
+;;   (when (load "flycheck" t t)
+;;     (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+;;     (add-hook 'elpy-mode-hook 'flycheck-mode)))
 
 ;; (use-package elpy
 ;;   :straight t
