96899c57c5ab22f2b885cb09bdbb044751e3c7f9
makutak <kouno@catallaxy.me>
2020-06-18 14:50:49 +0900

    docker関連の設定を追加

--- inits/20_docker.el
+++ inits/20_docker.el
@@ -1,6 +1,12 @@
 (use-package dockerfile-mode
-  :straight t)
+  :straight t
+  :mode (("Dockerfile\\'" . dockerfile-mode)))
 
 (use-package docker-compose-mode
   :straight t)
+
+(use-package docker-tramp
+  :straight t
+  :config
+  (set-variable 'docker-tramp-use-names t))
 
