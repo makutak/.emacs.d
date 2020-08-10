3f82969a51ad06d67782a165df32b6893dbc9a98
makutak <takkouno@gmail.com>
2020-08-10 23:42:11 +0900

    update

--- inits/20_python.el
+++ inits/20_python.el
@@ -1,38 +1,28 @@
 (use-package py-yapf
-  :straight t)
+  :straight t
+  :config
+  (add-hook 'python-mode-hook 'py-yapf-enable-on-save))
 
 (use-package python-mode
   :straight t
-  ;; :hook
-  ;; (python-mode . py-yapf-enable-on-save)
   :config
   (setq python-indent-offset 4))
 
 
-(use-package pipenv
-  :straight t
-  :hook (python-mode . pipenv-mode)
-  :init
-  (setq
-   pipenv-projectile-after-switch-function
-   #'pipenv-projectile-after-switch-extended))
+;; (use-package pipenv
+;;   :straight t
+;;   :hook (python-mode . pipenv-mode)
+;;   :init
+;;   (setq
+;;    pipenv-projectile-after-switch-function
+;;    #'pipenv-projectile-after-switch-extended))
 
+;; require `pip install yapf virtualenv'
 
 (use-package elpy
   :straight t
-  :defer t
   :init
-  (advice-add 'python-mode :before 'elpy-enable)
-  ;; :hook
-  ;; (elpy-mode . py-yapf-enable-on-save)
+  (elpy-enable)
   :config
-  (setq elpy-rpc-virtualenv-path 'current)
-  (when (load "flycheck" t t)
-    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-    (add-hook 'elpy-mode-hook 'flycheck-mode)))
+  (setq elpy-rpc-virtualenv-path 'current))
 
-;; (use-package elpy
-;;   :straight t
-;;   :defer t
-;;   :init
-;;   (advice-add 'python-mode :before 'elpy-enable))
