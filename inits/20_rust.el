126ebf686bf781675ace8ffec141746e50f949c1
makutak <takkouno@gmail.com>
2020-12-10 05:05:32 +0900

    use rust-analyzer

--- inits/20_rust.el
+++ inits/20_rust.el
@@ -1,6 +1,22 @@
-(use-package rustic
-  :straight t
-  :config
-  (yas-minor-mode 1)
-  (setq rustic-lsp-server 'rls)
-  (setq rustic-format-trigger 'on-save))
+(setq exec-path (cons (expand-file-name "~/.local/bin") exec-path))
+(setq exec-path (cons (expand-file-name "~/.cargo/bin") exec-path))
+
+(use-package rust-mode
+  :ensure t
+  :custom rust-format-on-save t)
+
+
+(use-package cargo
+  :ensure t
+  :hook (rust-mode . cargo-minor-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; #lsp
+
+(use-package lsp-mode
+  :ensure t
+  :hook (rust-mode . lsp)
+  :bind ("C-c h" . lsp-describe-thing-at-point)
+  :custom (lsp-rust-server 'rust-analyzer))
+(use-package lsp-ui
+  :ensure t)
