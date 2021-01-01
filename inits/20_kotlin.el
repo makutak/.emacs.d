7687e68c4f6e01fddcd363bb2434532eb7a7c237
makutak <takkouno@gmail.com>
2021-01-02 03:05:23 +0900

    不要

--- inits/20_kotlin.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'exec-path "~/work/kotlin-language-server/server/build/install/server/bin")
-
-(use-package kotlin-mode)
-(use-package flycheck-kotlin
-	:after kotlin-mode
-	:init
-	(flycheck-kotlin-setup))
