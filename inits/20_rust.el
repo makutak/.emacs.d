9a6cd4e83d98350f8b7176260a12905c97593037
makutak <takkouno@gmail.com>
2021-01-02 03:06:04 +0900

    修正

--- inits/20_rust.el
+++ inits/20_rust.el
@@ -1,25 +1,22 @@
-(setq exec-path (cons (expand-file-name "~/.local/bin") exec-path))
-(setq exec-path (cons (expand-file-name "~/.cargo/bin") exec-path))
-
-(use-package rust-mode
-  :ensure t
-  :custom rust-format-on-save t)
-
-
-(use-package cargo
-  :ensure t
-  :hook (rust-mode . cargo-minor-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; #lsp
-
 (use-package lsp-mode
-  :ensure t
+  :straight t
   :hook (rust-mode . lsp)
   :bind ("C-c h" . lsp-describe-thing-at-point)
   :custom (lsp-rust-server 'rust-analyzer))
 (use-package lsp-ui
-  :ensure t)
+  :straight t)
 
 (setq lsp-rust-analyzer-cargo-load-out-dirs-from-check t)
 (setq lsp-rust-analyzer-proc-macro-enable t)
+
+;; #rust
+(use-package rust-mode
+  :straight t
+  :custom rust-format-on-save t
+  :bind (:map rust-mode-map
+              ("M-RET" . lsp-execute-code-action)))
+
+(use-package cargo
+  :straight t
+  :hook (rust-mode . cargo-minor-mode))
