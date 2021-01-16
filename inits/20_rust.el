3f9c86abbda90c295db673889b539d3b2667b5d4
makutak <takkouno@gmail.com>
2021-01-17 03:41:06 +0900

    update

--- inits/20_rust.el
+++ inits/20_rust.el
@@ -1,19 +1,7 @@
-;;; #lsp
-(use-package lsp-mode
-  :straight t
-  :hook (rust-mode . lsp)
-  :bind ("C-c h" . lsp-describe-thing-at-point)
-  :custom (lsp-rust-server 'rust-analyzer))
-(use-package lsp-ui
-  :straight t)
-
-(setq lsp-rust-analyzer-cargo-load-out-dirs-from-check t)
-(setq lsp-rust-analyzer-proc-macro-enable t)
-
 ;; #rust
 (use-package rust-mode
   :straight t
-  :custom rust-format-on-save t
+  :custom (rust-format-on-save t)
   :bind (:map rust-mode-map
               ("M-RET" . lsp-execute-code-action)))
 
@@ -21,8 +9,21 @@
   :straight t
   :hook (rust-mode . cargo-minor-mode))
 
-(use-package flycheck-rust
+;;; #lsp
+(use-package lsp-mode
   :straight t
+  :hook (rust-mode . lsp)
+  :bind ("C-c h" . lsp-describe-thing-at-point)
+  :custom (lsp-rust-server 'rust-analyzer)
+  :config
+  (setq lsp-rust-analyzer-cargo-load-out-dirs-from-check t)
+  (setq lsp-rust-analyzer-proc-macro-enable t))
+(use-package lsp-ui
+  :straight t)
+
+(use-package flycheck-rust
   :config
   (with-eval-after-load 'rust-mode
-    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)))
+  
+
