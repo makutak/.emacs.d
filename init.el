fb002b988bca5deb9b9f926332a1753f5b19612c
makutak <takkouno@gmail.com>
2026-05-21 10:25:27 +0900

    fix

--- init.el
+++ init.el
@@ -258,8 +258,8 @@
   :if (eq system-type 'gnu/linux)
   :config
   (setq fcitx-use-dbus (and (boundp 'dbus-registered-buses)
-                             (executable-find "fcitx5-remote")
-                             (= 0 (call-process "fcitx5-remote" nil nil nil nil))))
+                            (executable-find "fcitx5-remote")
+                            (= 0 (call-process "fcitx5-remote" nil nil nil nil))))
   (setq fcitx-remote-command "fcitx5-remote")
   (fcitx-aggressive-setup))
 
